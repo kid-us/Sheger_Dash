9325e8c31fb051b82df3c812ca30f80f41601c5c
kid-us <kidushh29@gmail.com>
2024-09-02 00:03:20 +0300

    Dev't setup

---
+++ src/components/Pages/Home.tsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+  return (
+    <>
+      <div className="container">
+        <p>Welcome to Shegerlace Dashboard</p>
+      </div>
+    </>
+  );
+};
+
+export default Home;
