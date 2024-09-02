e243fe6e96b0004e48a59a6799bd29b9a978d6b8
kid-us <kidushh29@gmail.com>
2024-09-02 23:20:39 +0300

    Brands hook

--- src/components/Brands/Brands.tsx
+++ src/components/Brands/Brands.tsx
@@ -1,41 +1,16 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import baseUrl from "../../services/request";
-
-interface BrandItem {
-  id: number;
-  brand_names: string;
-}
-
-interface Brand {
-  brands: BrandItem[];
-}
+import useBrands from "../../hooks/useBrands";
 
 const Brands = () => {
-  const [brands, setBrands] = useState<string>("");
+  const [brand, setBrand] = useState<string>("");
   const [error, setError] = useState<boolean>(false);
-  const [brand, setBrand] = useState<BrandItem[]>([]);
-
-  // Get Brand
-  useEffect(() => {
-    axios
-      .get<Brand>(`${baseUrl}store/get-brands`, {
-        headers: {
-          "Content-Type": "application/json",
-          "ngrok-skip-browser-warning": "69420",
-        },
-      })
-      .then((response) => {
-        setBrand(response.data.brands);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  const { brands } = useBrands();
 
   // Create Brand
   const handleSubmit = () => {
-    if (brands.length < 3) {
+    if (brand.length < 3) {
       setError(true);
       return;
     }
@@ -43,7 +18,7 @@ const Brands = () => {
     setError(false);
 
     axios
-      .post(`${baseUrl}store/create-brand?brand=${brands}`, {
+      .post(`${baseUrl}store/create-brand?brand=${brand}`, {
         headers: {
           "Content-Type": "application/json",
         },
@@ -83,8 +58,8 @@ const Brands = () => {
           type="text"
           name="category"
           className="text-black bg-white rounded h-11 focus:outline-none ps-3 shadow shadow-zinc-900 w-80 block"
-          onChange={(e) => setBrands(e.currentTarget.value)}
-          value={brands}
+          onChange={(e) => setBrand(e.currentTarget.value)}
+          value={brand}
         />
         {error && (
           <p className="text-xs mt-1 text-red-700">Category required</p>
@@ -99,8 +74,8 @@ const Brands = () => {
         {/* List of Categories */}
         <p className="mt-7">List of active Brands</p>
         <div className="bg-white  rounded shadow shadow-zinc-900 px-4 pt-3 w-80 mt-3">
-          {brand.length > 0 ? (
-            brand.map((b) => (
+          {brands.length > 0 ? (
+            brands.map((b) => (
               <div
                 key={b.id}
                 className="grid grid-cols-12 hover:text-gray-400 mb-2"
---
+++ src/hooks/useBrands.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import baseUrl from "../services/request";
+
+export interface BrandItem {
+  id: number;
+  brand_names: string;
+}
+
+export interface Brand {
+  brands: BrandItem[];
+}
+
+const useBrands = () => {
+  const [brands, setBrands] = useState<BrandItem[]>([]);
+
+  useEffect(() => {
+    // Fetch Brands
+    const fetchBrands = async () => {
+      try {
+        const response = await axios.get<Brand>(`${baseUrl}store/get-brands`, {
+          headers: {
+            "Content-Type": "application/json",
+            "ngrok-skip-browser-warning": "69420",
+          },
+        });
+        setBrands(response.data.brands);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchBrands();
+  }, []);
+
+  return {
+    brands,
+  };
+};
+
+export default useBrands;
